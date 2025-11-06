bab7825bc1b3463b014785bd2671925563496ce6
Marshal <marshalolimmah@gmail.com>
2025-11-06 15:26:59 -0600

    CHanged something

--- Chapter1Basics/Test_1.py
+++ Chapter1Basics/Test_1.py
@@ -2,7 +2,7 @@
 print('Hello, World!')
 print('Hello, My Name is Marshal and I am a man!')
 print('My name is Marshal Chinedu Olimmah, Check me out')
-Print('Now it has finally committed to Github')
+print('Now it has finally committed to Github')
 
 print('That was something')
 
