d627de48a6b291e9da19b5c04651f77098f4f86b
Marshal <marshalolimmah@gmail.com>
2025-11-06 17:01:19 -0600

    Updated with a new file named hello.py to test python

--- Chapter1Basics/Test_1.py
+++ Chapter1Basics/Test_1.py
@@ -1,9 +1,10 @@
+print("Hello, World!")
+print("Hello, My Name is Marshal and I am a man!")
+print("My name is Marshal Chinedu Olimmah, Check me out")
+print("Now it has finally committed to Github")
 
-print('Hello, World!')
-print('Hello, My Name is Marshal and I am a man!')
-print('My name is Marshal Chinedu Olimmah, Check me out')
-print('Now it has finally committed to Github')
+print("That was something")
 
-print('That was something')
+print("One thing to note is that the MU does not update like Visio code")
 
-print('One thing to note is that the MU does not update like Visio code')
+
---
+++ Chapter1Basics/hello.py
@@ -0,0 +1,16 @@
+# This is a program that says hello and asks for my name. #
+
+print('Hello, World!')
+print()  ## This adds a blank line into the output for better readability
+print('What is your name? ')  # Ask for their name.
+print()  ## This adds a blank line into the output for better readability
+my_name = input('Please tell me your age after this prompt >>>>>> ')
+print('It is good to meet you, ' + my_name)
+print('The length of your name is: ')
+print(len(my_name))
+print('What is your age? ') # Ask users for their age.
+my_age = input('Please tell me your age after this prompt >>>>>>> ')
+print()
+print('You will be ' + str(int(my_age) + 1) + ' in a year.')
+# print(f'You will be {int(my_age) +1} in a year')  # This format makes the result cleaner
+
