5b5345eb6ef753b3f8740ed302dd356c07a40528
Marshal <marshalolimmah@gmail.com>
2025-11-06 14:43:05 -0600

    Second Addition of code.

--- Chapter1Basics/Test_1.py
+++ Chapter1Basics/Test_1.py
@@ -1,3 +1,4 @@
 # Write yoprint('Hello, World!')ur code here :-)
 print('Hello, World!')
 print('Hello, My Name is Marshal and I am a man!')
+print('My name is Marshal Chinedu Olimmah, Check me out')
